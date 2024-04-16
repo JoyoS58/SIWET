b34cbc41186a615cb5ca7956e964bbc80655946b
JoyoS58 <joyosugito99@gmail.com>
2024-04-17 05:54:39 +0700

    Fitur Keuangan RW

---
+++ resources/views/keuanganRW/index.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.template')
+@section('title', 'Pengelolaan Kas RW')
+
+@section('content_header')
+    <h1>Pengelolaan Kas RW</h1>
+@endsection
+@section('')
+@endsection
+@section('content')
+<head>
+<H1>PENGELOLAAN KAS RW</H1>
+</head>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Pengelolaan Kas Rukun Warga</h3>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID Transaksi</th>
+                                <th>Jenis Transaksi</th>
+                                <th>Nominal</th>
+                                <th>Tanggal Transaksi</th>
+                                <th>Keterangan</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <BR></BR>
+                        <tbody>
+                            <tr>
+                                <td>01</td>
+                                <td>Pemasukan</td>
+                                <td>100.000</td>
+                                <td>2 April 2023</td>
+                                <td>Pembayaran Kas</td>
+                                <td class="action-buttons">
+                                    <button class="edit-button"><a href="#">Edit</a></button>
+                                    <button class="delete-button"><a href="#" >Delete</a></button>
+                                    <button class="detail-button"><a href="#" >Detail</a></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>02</td>
+                                <td>Pengeluaran</td>
+                                <td>50.000</td>
+                                <td>3 Apr 2023</td>
+                                <td>Dana Kerja Bakti</td>
+                                <td class="action-buttons2">
+                                    <button class="edit-button"><a href="#">Edit</a></button>
+                                    <button class="delete-button"><a href="#" >Delete</a></button>
+                                    <button class="detail-button"><a href="#" >Detail</a></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>03</td>
+                                <td>Pemasukan</td>
+                                <td>100.000</td>
+                                <td>5 Apr 2023</td>
+                                <td>Pembayaran Kas</td>
+                                <td class="action-buttons">
+                                    <button class="edit-button"><a href="#">Edit</a></button>
+                                    <button class="delete-button"><a href="#" >Delete</a></button>
+                                    <button class="detail-button"><a href="#" >Detail</a></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>04</td>
+                                <td>Pengeluaran</td>
+                                <td>50.000</td>
+                                <td>6 April 2023</td>
+                                <td>Pembayaran Sampan</td>
+                                <td class="action-buttons">
+                                    <button class="edit-button"><a href="#">Edit</a></button>
+                                    <button class="delete-button"><a href="#" >Delete</a></button>
+                                    <button class="detail-button"><a href="#" >Detail</a></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>05</td>
+                                <td>Pemasukan</td>
+                                <td>200.000</td>
+                                <td>7 April 2023</td>
+                                <td>Sumbangan Acara Maulid Nabi</td>
+                                <td class="action-buttons">
+                                    <button class="edit-button"><a href="#">Edit</a></button>
+                                    <button class="delete-button"><a href="#" >Delete</a></button>
+                                    <button class="detail-button"><a href="#" >Detail</a></button>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <br>
+                        <tfoot>
+                            <tr>
+                                <th colspan="5">TOTAL KAS</th>
+                                <th></th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
