7b49b01fdc070cc2acd8eebb81bd30273a4fca93
JoyoS58 <joyosugito99@gmail.com>
2024-05-26 20:29:43 +0700

    .test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,12 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use app\Models\AnggotaPKK;
+use app\Models\KegiatanPKK;
+use app\Models\KegiatanRW;
+use app\Models\KeuanganRW;
+use app\Models\KeuanganPKK;
+use app\Models\MahasiswaKos;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +18,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        AnggotaPKK::factory(10)->create();
+        KegiatanPKK::factory(10)->create();
+        KegiatanRW::factory(10)->create();
+        AnggotaPKK::factory(10)->create();
+        MahasiswaKos::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
